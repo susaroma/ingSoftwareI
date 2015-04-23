a495536aab0ae78e9161c30eaf08cb87b77fd392
susyroma <susyroma@localhost>
2015-04-22 21:45:08 -0430

    Casos de PRueba 1

--- Tarea2/testCalcularPrecio.py
+++ Tarea2/testCalcularPrecio.py
@@ -6,14 +6,31 @@ Created on 22/4/2015
 '''
 from calcularPrecio import *
 import unittest
+from datetime import *
 
 
 class CasosDePrueba(unittest.TestCase):
-
-
-    def ejecutarCalcularPrecio(self):
-        pass
-
+    
+    def test0Timer(self):
+        # Caso de prueba tiempo de reservacion = (0) minutos
+        tarifaPrueba = Tarifa(15,17)
+        reservaIni = datetime(2015, 4, 23, 9, 20, 0, 0)
+        reservaFin = datetime(2015, 4, 23, 9, 20, 0, 0)
+        tiempoReserva = [reservaIni,reservaFin]
+        try:
+            calcularPrecio(tarifaPrueba, tiempoReserva)
+        except:
+            pass
+    
+    def test15MTimeSmallT(self):
+        # Caso de prueba tiempo de reservacion = (15) minutos, tarifa pequenia
+        tarifaPrueba = Tarifa(1.75, 7)
+        reservaIni = datetime(2015, 4, 21, 6, 15, 0, 0)
+        reservaFin = datetime(2015, 4, 21, 6, 30, 0, 0)
+        tiempoReserva = [reservaIni,reservaFin]
+        precio = calcularPrecio(tarifaPrueba, tiempoReserva)
+        self.assertEqual(precio, 1.75) 
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']

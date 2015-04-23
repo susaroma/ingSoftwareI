993fa0ce42a58c9b5ae317451c622f6e376ba07e
Marisela Del Valle <marisela.delvalle93@gmail.com>
2015-04-23 07:32:10 -0430

    primera prueba merge conflict

--- Tarea2/testCalcularPrecio.py
+++ Tarea2/testCalcularPrecio.py
@@ -4,18 +4,13 @@ ll
 @author: susyroma
 '''
 import unittest
-from calcularPrecio import *
-from datetime import *
+
 
 class Test(unittest.TestCase):
 
 
-    def testFinSemana(self):
-        tarifa = Tarifa (25,45)
-        inicioDeReservacion = datetime(2015,4,25,12,30,0,0)
-        finDeReservacion = datetime(2015,4,26,12,30,0,0)
-        tiempoDeReservacionr = [inicioDeReservacion, finDeReservacion]
-        self.assertTrue(1080 == self.calcularPrecio(tarifa,tiempoDeReservacionr)
+    def testCalculaPrecio(self):
+        pass
 
 
 if __name__ == "__main__":
